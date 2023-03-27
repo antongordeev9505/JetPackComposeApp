07e53c0f4f7c5eeaa4f8ed8601d267bcd834ba14
Anton Gordeev <aigordeev@alfabank.ru>
2023-03-27 16:42:50 +0300

    Created LazyColumn

---
+++ app/src/main/java/com/example/jetpackcompose/ui/components/CardComponent.kt
@@ -0,0 +1,64 @@
+package com.example.jetpackcompose.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.R
+import com.example.jetpackcompose.sample.MyCard
+
+@Composable
+fun CardComponent(item: MyCard) {
+    Card(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .height(IntrinsicSize.Min)
+        ) {
+            Row(
+                modifier = Modifier.weight(1f),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = item.title, modifier = Modifier.weight(1f))
+                Divider(modifier = Modifier.fillMaxHeight().width(1.dp))
+                Icon(
+                    painter = painterResource(
+                        id = if (item.isDone) {
+                            R.drawable.ic_baseline_done_24
+                        } else {
+                            R.drawable.ic_outline_close_24
+                        }
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+            Divider()
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = item.description)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/jetpackcompose/ui/screens/screen1/views/LisView.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screens/screen1/views/LisView.kt
@@ -1,65 +1,22 @@
 package com.example.jetpackcompose.ui.screens.screen1.views
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.jetpackcompose.R
 import com.example.jetpackcompose.sample.MyCard
+import com.example.jetpackcompose.ui.components.CardComponent
 
 @Composable
-fun ListView(data: List<MyCard>) {
-    val item = data.first()
-    Card(
-        modifier = Modifier.padding(16.dp),
-        shape = RoundedCornerShape(16.dp)
+fun ListView(list: List<MyCard>) {
+    LazyColumn(
+        contentPadding = PaddingValues(vertical = 16.dp, horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-                .height(IntrinsicSize.Min)
-        ) {
-            Row(
-                modifier = Modifier.weight(1f),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = item.title, modifier = Modifier.weight(1f))
-                Divider(modifier = Modifier.fillMaxHeight().width(1.dp))
-                Icon(
-                    painter = painterResource(
-                        id = if (item.isDone) {
-                            R.drawable.ic_baseline_done_24
-                        } else {
-                            R.drawable.ic_outline_close_24
-                        }
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier.size(32.dp)
-                )
-            }
-            Divider()
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(text = item.description)
-            }
+        items(list) { card ->
+            CardComponent(card)
         }
     }
 }
